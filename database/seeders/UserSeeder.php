cf0db0ebb4842fda2c37be71cfee589f4ccefb15
Curtis Delicata <curtis.delicat@outlook.com>
2026-03-20 14:55:06 +0000

    Update UserSeeder and RoleSeeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -8,6 +8,7 @@ use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class UserSeeder extends Seeder
 {
